e30cfb58cb80a44087535e5cd0e3bd76e1b47756
douglas1001 <douglassilva1001@gmail.com>
2025-08-04 18:18:52 -0300

    Inclusao da logica

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-consoele.log("Teste Primeiro Desafio")
-console.log("Teste 2")
+let NomeHeroi = Douglas ;
+let ExperienciaHeori = 1000;
+
+if (ExperienciaHeori <= 1000){
+    console.log("O Herói de nome "+ NomeHeroi +" está no nível de Ferro")
+}else if (ExperienciaHeori < 1000 && ExperienciaHeori <= 2000 ){
+    console.log("O Herói de nome "+ NomeHeroi +" está no nível de Bronze")
+}
